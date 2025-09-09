50155b96bb188384bd531788b707058008d08f78
unknown <istartgame31@gmail.com>
2025-09-09 23:29:04 +0700

    making progress

--- src/manage_plugin/remove_plugin.rs
+++ src/manage_plugin/remove_plugin.rs
@@ -1,10 +1,16 @@
 use std::path::{PathBuf};
 use std::fs;
+use serde_json::{ from_str };
+use std::str::{from_utf8};
+use serde::{Deserialize, Serialize};
 
 use crate::{ DEFAULT_PLUGIN_DIRECTORY };
 
 
-
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PluginInfo {
+    pub plugin_path: String
+}
 
 pub fn new(plugin_id: &str) -> Result<(), Box<dyn std::error::Error>> {
     let plugin_dir = PathBuf::from(std::env::var("PLUGIN_DIRECTORY").unwrap_or(DEFAULT_PLUGIN_DIRECTORY.to_string()));
@@ -13,7 +19,23 @@ pub fn new(plugin_id: &str) -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let tree = sled::open(&plugin_dir.join("manifest"))?;
+    if let Some(value)= tree.get(plugin_id.as_bytes())? {
+        
+        let value: PluginInfo = from_str(from_utf8(&value)?)?;
+
+        let plugin_path = PathBuf::from(&value.plugin_path);
+
+        if plugin_path.exists() {
+            fs::remove_file(&plugin_path).unwrap();
+        }
+
+
+    }else{
+        return Err(format!("Plugin not ({}) found", plugin_id).into());
+    }
+
     tree.remove(plugin_id.as_bytes())?;
+
 
     return Ok(());
     
