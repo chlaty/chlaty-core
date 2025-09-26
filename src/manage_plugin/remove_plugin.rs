72692d97801128986d20cb9e5231f972321cc675
unknown <istartgame31@gmail.com>
2025-09-26 16:56:55 +0700

    modify remove_plugin

--- src/manage_plugin/remove_plugin.rs
+++ src/manage_plugin/remove_plugin.rs
@@ -1,7 +1,12 @@
 
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use std::fs;
 
 use crate::utils::manifest::remove;
+use crate::utils::get_lib_extension;
+
+use crate::DEFAULT_PLUGIN_DIRECTORY;
 
 
 
@@ -14,6 +19,19 @@ pub fn new(source: &str, plugin_id: &str) -> Result<(), Box<dyn std::error::Erro
     
 
     remove(source, plugin_id)?;
+
+    let plugin_dir = PathBuf::from(std::env::var("CHLATY_PLUGIN_DIRECTORY").unwrap_or(DEFAULT_PLUGIN_DIRECTORY.to_string()));
+        
+    let source_dir = plugin_dir.join(source);
+
+    let lib_extension = get_lib_extension::new()?;
+    let file_name = format!("lib-{}{}", &plugin_id, &lib_extension);
+
+    let file_path = source_dir.join(file_name);
+
+    if file_path.exists() {
+        fs::remove_file(file_path)?;    
+    }
 
     return Ok(());
     
--- src/test.rs
+++ src/test.rs
@@ -19,34 +19,34 @@ mod tests {
     //     }
     // }
 
-    use crate::manage_plugin::{
-        install_plugin,
-        install_plugin::PluginManifest
-    };
+    // use crate::manage_plugin::{
+    //     install_plugin,
+    //     install_plugin::PluginManifest
+    // };
 
-    #[test]
-    fn test_download_plugin() -> Result<(), Box<dyn std::error::Error>> {
-        dotenv().ok();
-        let result = install_plugin::new(
-            "anime",
-            "hianime", 
-            "latest",
-            PluginManifest{
-                title: "HiAnime".to_string(),
-                manifest: "https://raw.githubusercontent.com/chlaty/chlaty-lib-hianime/refs/heads/main/manifes.json".to_string()
-            },
-            |_, _| {}
-        );
-        match result {
-            Ok(data) => {
-                println!("Test [get installed plugin list] passed with result: {:?}", data);
-                return Ok(().into());
-            },
-            Err(e) => {
-                return Err(e.into());
-            },
-        }
-    }
+    // #[test]
+    // fn test_download_plugin() -> Result<(), Box<dyn std::error::Error>> {
+    //     dotenv().ok();
+    //     let result = install_plugin::new(
+    //         "anime",
+    //         "hianime", 
+    //         "latest",
+    //         PluginManifest{
+    //             title: "HiAnime".to_string(),
+    //             manifest: "https://raw.githubusercontent.com/chlaty/chlaty-lib-hianime/refs/heads/main/manifes.json".to_string()
+    //         },
+    //         |_, _| {}
+    //     );
+    //     match result {
+    //         Ok(data) => {
+    //             println!("Test [get installed plugin list] passed with result: {:?}", data);
+    //             return Ok(().into());
+    //         },
+    //         Err(e) => {
+    //             return Err(e.into());
+    //         },
+    //     }
+    // }
 
     // use crate::manage_plugin::get_installed_plugin_list;
 
@@ -124,7 +124,7 @@ mod tests {
     // #[test]
     // fn request_plugin_get_episode_server() -> Result<(), Box<dyn std::error::Error>> {
     //     dotenv().ok();
-    //     let result = get_episode_server::new("anime", "hianime", "100");
+    //     let result = get_episode_server::new("anime", "hianime", "0", "100");
     //     match result {
     //         Ok(data) => {
     //             println!("Test [request plugin: get_episode_server] passed with result: {:?}", data);
